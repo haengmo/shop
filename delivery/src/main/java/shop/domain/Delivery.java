8f328c051cac030c3e62a641fec44121d2aad68a
haengmo <145944034+haengmo@users.noreply.github.com>
2025-07-07 13:54:26 +0900

    commit

---
+++ delivery/src/main/java/shop/domain/Delivery.java
@@ -0,0 +1,74 @@
+package shop.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.*;
+import lombok.Data;
+import shop.DeliveryApplication;
+import shop.domain.DeliberyCancelled;
+import shop.domain.DeliveryStarted;
+
+@Entity
+@Table(name = "Delivery_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String orderid;
+
+    private String customerid;
+
+    private String productid;
+
+    private Integer qty;
+
+    private String address;
+
+    private String status;
+
+    public static DeliveryRepository repository() {
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(
+            DeliveryRepository.class
+        );
+        return deliveryRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void startDelibery(OrderPlaced orderPlaced) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
+        deliveryStarted.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+
+        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+            DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
+            deliveryStarted.publishAfterCommit();
+
+         });
+        */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
